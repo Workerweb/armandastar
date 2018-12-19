7500bcdb2d38cfcb5ca327f2f94a690c3d45d900
Dima <marthmarth123@gmail.com>
2018-12-19 22:19:06 +0600

    *Настроена страница в разработке *Прописаны стили *Добавлены материалы *Прочие изменения

---
+++ resources/views/pages/dev/dev.blade.php
@@ -0,0 +1,24 @@
+@extends('pages.layouts.app')
+
+
+@section('title', 'Страница находится в разработке')
+
+@section('content')
+
+	<div class="container-fluid dev-block">
+        <div class="row dev-block__row">
+
+        	<div class="col-md-4 offset-md-1 dev-block__left">
+        		<p class="dev-block__text">страница  в разработке</p>
+        	</div>
+
+        	<div class="col-md-6 dev-block__right">
+        		<img src="/img/dev/dev-image.png" alt="" class="dev-block__image">
+        	</div>
+                
+                
+        </div>
+    </div>
+
+
+@endsection
