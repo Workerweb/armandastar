9f91c9c97a73bcdeccef6c7b40b4afc3b07275ab
Dima <marthmarth123@gmail.com>
2018-12-19 21:48:06 +0600

    *Удалены старые изображения для меню *Добавлен новые изображения для мен *Сделана верстка нового dropdown menu

---
+++ resources/views/pages/layouts/partials/drop-menu.blade.php
@@ -0,0 +1,141 @@
+ <div class="col-md-12 drop-menu_wrap">
+
+    <!-- *********************************REPEATED PART*****************************************-->
+    <div class="drop-menu">
+
+        <a class="drop-menu__block active" href="/hero_aliya">
+            <p class="drop-menu__block-text">Алия <br>Токтасынова</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_1.png" alt="">
+        </a>
+        
+        <a 
+        class="drop-menu__block active" href="/hero_george">
+            <p class="drop-menu__block-text">Георгий <br>Патриашвили</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_2.png" alt="">
+        </a>
+        
+        <a 
+        class="drop-menu__block active" href="/hero_rustem">
+            <p class="drop-menu__block-text">Рустем  <br>Сундетов</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_3.png" alt="">
+        </a>
+        
+        <a 
+        class="drop-menu__block" href="/">
+            <p class="drop-menu__block-text">Тимур <br>Коробейников</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_4.png" alt="">
+        </a>
+        
+        <a 
+        class="drop-menu__block" href="/">
+            <p class="drop-menu__block-text">Камиля  <br>Сулейменова</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_5.png" alt="">
+        </a>
+        <!-- *********************************REPEATED PART*****************************************-->
+
+
+        
+        <!-- *********************************REPEATED PART*****************************************-->
+            <a class="drop-menu__block" href="/">
+            <p class="drop-menu__block-text">Данияр <br>Айтышев</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_6.png" alt="">
+        </a>
+        
+        <a 
+        class="drop-menu__block" href="/">
+            <p class="drop-menu__block-text">Самсон  <br>Аракелян</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_7.png" alt="">
+        </a>
+        
+        <a 
+        class="drop-menu__block" href="/">
+            <p class="drop-menu__block-text">Нурлан <br>Калтаев</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_8.png" alt="">
+        </a>
+        
+        <a 
+        class="drop-menu__block" href="/">
+            <p class="drop-menu__block-text">Жулдыз <br>Шабденова</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_9.png" alt="">
+        </a>
+        
+        <a 
+        class="drop-menu__block" href="/">
+            <p class="drop-menu__block-text">Адлет и Даулет  <br>Анарбековы</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_10.png" alt="">
+        </a>
+        
+        <!-- 
+            *********************************REPEATED PART*****************************************-->
+
+
+        <!-- *********************************REPEATED PART*****************************************-->
+        <a 
+        class="drop-menu__block" href="/">
+            <p class="drop-menu__block-text">Саида и Дина <br>Ыбышевы</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_11.png" alt="">
+        </a>
+        
+        <a 
+        class="drop-menu__block" href="/">
+            <p class="drop-menu__block-text">Геннадий <br>Мороз</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_12.png" alt="">
+        </a>
+        
+        <a 
+        class="drop-menu__block" href="/">
+            <p class="drop-menu__block-text">Шах-Мурат <br>Ордабаев</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_13.png" alt="">
+        </a>
+        
+        <a 
+        class="drop-menu__block" href="/">
+            <p class="drop-menu__block-text">Александр <br>Пупенко</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_14.png" alt="">
+        </a>
+        
+        <a 
+        class="drop-menu__block" href="/">
+            <p class="drop-menu__block-text">Данияр <br>Жакипбаев</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_15.png" alt="">
+        </a>
+        
+        <!-- 
+            *********************************REPEATED PART*****************************************-->
+
+
+
+        <!-- *********************************REPEATED PART*****************************************-->
+        <a class="drop-menu__block" href="/">
+            <p class="drop-menu__block-text">Самал <br>Еслямова</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_16.png" alt="">
+        </a>
+        
+        <a 
+        class="drop-menu__block" href="/">
+            <p class="drop-menu__block-text">Ержан <br>Танаев</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_17.png" alt="">
+        </a>
+        
+        <a 
+        class="drop-menu__block" href="/">
+            <p class="drop-menu__block-text">Тамила <br>Муридова</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_18.png" alt="">
+        </a>
+        
+        <a 
+        class="drop-menu__block" href="/">
+            <p class="drop-menu__block-text">Гордей <br>Трищенков</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_19.png" alt="">
+        </a>
+        
+        <a 
+        class="drop-menu__block" href="">
+            <p class="drop-menu__block-text">Людмила <br>Куфонина</p>
+            <img class="drop-menu__block-image" src="/img/menu/hero_20.png" alt="">
+        </a>
+        
+        <!-- 
+            *********************************REPEATED PART*****************************************-->
+    </div>
+</div>
--- resources/views/pages/layouts/partials/main-video.blade.php
+++ resources/views/pages/layouts/partials/main-video.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid video-block">
     <div class="row">
         <div class="col-md-12">
-        	<video class="video-block__item" id="poster-video" poster="/img/main/poster.jpg" autoplay="" muted="" loop="">
+        	<video class="video-block__item" id="poster-video" poster="/img/main/poster.jpg" autoplay muted loop>
                 <source src="/video/video_ru.mp4" type="video/mp4">
             </video>
         </div>
